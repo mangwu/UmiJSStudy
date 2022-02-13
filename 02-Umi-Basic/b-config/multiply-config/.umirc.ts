bc36c715bbb742c5bad96d977fd3b8cb775807de
mangwu <1185956753@qq.com>
2022-02-14 02:33:09 +0800

    多环境多配置

---
+++ 02-Umi-Basic/b-config/multiply-config/.umirc.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'umi';
+
+export default defineConfig({
+  nodeModulesTransform: {
+    type: 'none',
+  },
+  routes: [
+    { path: '/', component: '@/pages/index' },
+    // { path: '/hello', component: '@/pages/hello/index', exact: false },
+  ],
+  fastRefresh: {},
+});
---
+++ 02-Umi-Basic/b-config/multiply-config/src/pages/hello/index.tsx
@@ -0,0 +1,9 @@
+import styles from './index.less';
+
+export default function HelloWorld() {
+  return (
+    <div>
+      <h1 className={styles.title}>HelloWorld</h1>
+    </div>
+  );
+}
