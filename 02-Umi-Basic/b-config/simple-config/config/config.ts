102bab01eea9346c34953513c127b1a33978a975
mangwu <1185956753@qq.com>
2022-02-14 00:05:50 +0800

    将.umirc.ts删除，使用config/config.ts作为配置文件

--- 02-Umi-Basic/b-config/simple-config/config/config.ts
+++ 02-Umi-Basic/b-config/simple-config/config/config.ts
@@ -1,11 +1,9 @@
 import { defineConfig } from 'umi';
-
+import routes from './routes';
 export default defineConfig({
   nodeModulesTransform: {
     type: 'none',
   },
-  routes: [
-    { path: '/', component: '@/pages/index' },
-  ],
+  routes: routes,
   fastRefresh: {},
 });
