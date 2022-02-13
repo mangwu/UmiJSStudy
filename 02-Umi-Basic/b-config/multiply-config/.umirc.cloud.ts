662a083bf97bc8e61a8e2e5c6f482ab1c479f505
mangwu <1185956753@qq.com>
2022-02-14 02:45:15 +0800

    自定义配置+默认配置+临时配置属于deep merge

--- 02-Umi-Basic/b-config/multiply-config/.umirc.cloud.ts
+++ 02-Umi-Basic/b-config/multiply-config/.umirc.cloud.ts
@@ -5,7 +5,6 @@ export default defineConfig({
     type: 'none',
   },
   routes: [
-    { path: '/', component: '@/pages/index' },
     { path: '/hello', component: '@/pages/hello/index', exact: false },
   ],
   fastRefresh: {},
