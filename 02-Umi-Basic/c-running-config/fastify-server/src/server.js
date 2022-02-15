a82c7fc1bfb1271b9e87e6c59cf4affa11a3ed46
mangwu <1185956753@qq.com>
2022-02-15 20:02:58 +0800

    路由信息请求

--- 02-Umi-Basic/c-running-config/fastify-server/src/server.js
+++ 02-Umi-Basic/c-running-config/fastify-server/src/server.js
@@ -2,7 +2,7 @@
  * @Author: mangwu                                                             *
  * @File: server.js                                                            *
  * @Date: 2022-02-15 15:35:46                                                  *
- * @LastModifiedDate: 2022-02-15 18:23:13                                      *
+ * @LastModifiedDate: 2022-02-15 19:58:33                                      *
  * @ModifiedBy: mangwu                                                         *
  * -----------------------                                                     *
  * Copyright (c) 2022 inspur                                                   *
@@ -33,7 +33,7 @@ fastify.get("/auth", function (request, reply) {
 });
 fastify.post("/login", function (request, reply) {
   console.log(request.body, request.body);
-  const data = request.body
+  const data = request.body;
   if (data.username == "admin" && data.password == "admin") {
     isAuthed = true;
   } else {
@@ -41,7 +41,20 @@ fastify.post("/login", function (request, reply) {
   }
   reply.send({ isAuthed });
 });
-
+fastify.post("/route", function (request, reply) {
+  console.log(request.body);
+  const data = request.body;
+  if (data.role == "admin") {
+    // 管理员页面路由
+    reply.send([
+      { name: "管理", path: "/management" },
+      { name: "你好", path: "/hello" },
+    ]);
+  } else {
+    // 普通页面路由
+    reply.send([{ name: "你好", path: "/hello" }]);
+  }
+});
 /**
  * @description 启动服务
  * @argument {Number} Port 端口号号
@@ -56,10 +69,9 @@ fastify.listen(3000, function (err, address) {
   }
   // 服务器监听地址:${address}
   fastify.log.info(`server listening on ${address}`);
-  
 });
 
 // 模拟设置退出登录时间
-setInterval(()=> {
+setInterval(() => {
   isAuthed = false;
-}, 10000)
+}, 10000);
---
+++ 02-Umi-Basic/c-running-config/render-config-2/src/pages/Hello/index.tsx
@@ -0,0 +1,13 @@
+/*
+ * @Author: mangwu
+ * @File: index.tsx
+ * @Date: 2022-02-15 20:01:25
+ * @LastModifiedDate: 2022-02-15 20:01:27
+ * @ModifiedBy: mangwu
+ * -----------------------
+ * Copyright (c) 2022 inspur
+ * -----------------------
+ * @HISTORY:
+ * Date   	            By 	    Comments
+ * ---------------------	--------	-----------------------------------------------
+ */
