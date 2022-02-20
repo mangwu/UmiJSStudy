ec998d2dc79915bea2274fffe9ce1cbe22d69342
mangwu <1185956753@qq.com>
2022-02-20 23:53:19 +0800

    动态主题

--- 02-Umi-Basic/c-running-config/root-container-config/.umirc.ts
+++ 02-Umi-Basic/c-running-config/root-container-config/.umirc.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from 'umi';
+import { theme } from './src/theme/index';
 
 export default defineConfig({
   nodeModulesTransform: {
     type: 'none',
   },
-  routes: [
-    { path: '/', component: '@/pages/index' },
-  ],
+  routes: [{ path: '/', component: '@/pages/index' }],
   fastRefresh: {},
+  theme,
 });
--- 02-Umi-Basic/c-running-config/root-container-config/src/pages/index.tsx
+++ 02-Umi-Basic/c-running-config/root-container-config/src/pages/index.tsx
@@ -1,9 +1,11 @@
-import styles from './index.less';
+import './index.less';
 
 export default function IndexPage() {
   return (
     <div>
-      <h1 className={styles.title}>Page index</h1>
+      <h1 className="title">Page index</h1>
+      <h2 className="tishi">提示色</h2>
+      <h2 className='highlight'>高亮色</h2>
     </div>
   );
 }
